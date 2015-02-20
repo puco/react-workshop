92144f629121f532ad07761675311287a508fb90
Martin Pernecky <mpe@auctionata.com>
2015-02-20 15:38:40 +0100

    excercise 1

--- excercises/1-your-first-component/app.js
+++ excercises/1-your-first-component/app.js
@@ -21,9 +21,34 @@ var DATA = {
   ]
 };
 
+var MenuItem = React.createClass({
+  render: function () {
+    return (
+      <li>{this.props.name}</li>
+    );
+  }
+})
+
 var Menu = React.createClass({
   render: function () {
-    return null;
+    DATA.items.sort(sortBy('name'));
+    var rows = [];
+    DATA.items
+      .filter(function(i) {
+        return i.type === 'mexican';
+      })
+      .forEach(function(i) {
+        rows.push(<MenuItem name={i.name} />);
+      });
+
+    return (
+      <div class="menu">
+        <h1>{this.props.title}</h1>
+        <ul>
+          {rows}
+        </ul>
+      </div>
+    );
   }
 });
 
@@ -31,3 +56,57 @@ React.render(<Menu/>, document.body, () => {
   require('./tests').run();
 });
 
+
+// React.render(<div>hello</div>, document.body);
+// React.render(React.DOM.div({}, 'test2'), document.body);
+
+// var App = React.createClass({
+//   render: function() {
+//     var name = "test2";
+//     return (
+//       <div className="App">
+//         <h1>{this.props.test ? 'yes' : 'no'}</h1>
+//         <hr />
+//         {this.props.children}
+//         <hr />
+//         <pre>{JSON.stringify(this.props.children, undefined, 2)}</pre>
+//       </div>
+//     );
+//   }
+// });
+
+// var ContentToggle = React.createClass({
+
+//   getInitialState: function() {
+//     return {
+//       showDetails: true
+//     };
+//   },
+
+//   handleClick: function(event) {
+//     console.log('click', event);
+//     this.setState({
+//       showDetails: !this.state.showDetails
+//     });
+//   },
+
+//   render: function() {
+//     var details = this.state.showDetails ? this.props.children : null;
+//     return (
+//       <div class="ContentToggle">
+//         <div onClick={this.handleClick}>
+//           <h1>{this.props.summary} - {this.state.showDetails}</h1>
+//           {details}
+//         </div>
+//       </div>
+//     );
+//   }
+// });
+
+// React.render(
+//   <App test="xasdadasdad">
+//     <ContentToggle summary="TOGGLE">
+//       INSIDE
+//     </ContentToggle>
+//   </App>
+// , document.body);
